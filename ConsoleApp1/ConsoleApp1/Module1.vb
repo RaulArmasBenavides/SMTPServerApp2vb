fc98158cd60fbf383891b38168815f6a2b2a3128
Raul Armas <you@example.com>
2023-05-15 08:16:50 -0500

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Threading
+Imports SmtpServer
+
+Module Module1
+
+    Sub Main()
+        Dim optionsBuilder As New SmtpServerOptionsBuilder()
+        optionsBuilder.ServerName("localhost")
+        optionsBuilder.Port(25)
+        optionsBuilder.MessageStore(New SampleMessageStore())
+        optionsBuilder.MailboxFilter(New SampleMailboxFilter())
+        optionsBuilder.UserAuthenticator(New SampleUserAuthenticator())
+        Dim options = optionsBuilder.Build()
+
+        Dim smtpServer As SmtpServer.SmtpServer = New SmtpServer.SmtpServer(options)
+        smtpServer.StartAsync(CancellationToken.None)
+        Console.WriteLine("Servidor SMTP iniciado. Presione cualquier tecla para detener...")
+        Console.ReadKey()
+    End Sub
+
+End Module
